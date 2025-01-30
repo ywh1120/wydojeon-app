5415c8fb03115ac3908776a274e31ac81b774b87
ywh1120 <ywh1120@gmail.com>
2025-01-30 22:20:59 +0900

    first commit

---
+++ app/(dashboard)/orders/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+
+export default function OrdersPage() {
+  
+
+  return (
+    <Typography>
+      Welcome to the Toolpad orders!
+    </Typography>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { NextAppProvider } from '@toolpad/core/nextjs';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LinearProgress from '@mui/material/LinearProgress'
+import type { Navigation } from '@toolpad/core/AppProvider';
+
+import theme from '../theme';
+
+const NAVIGATION: Navigation = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    segment: '',
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: 'orders',
+    title: 'Orders',
+    icon: <ShoppingCartIcon />,
+  },
+];
+
+const BRANDING = {
+  title: 'My Toolpad Core Next.js App',
+};
+
+
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  
+
+  return (
+    <html lang="en" data-toolpad-color-scheme="light" suppressHydrationWarning>
+      <body>
+        
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <React.Suspense fallback={<LinearProgress />}>
+            <NextAppProvider
+              navigation={NAVIGATION}
+              branding={BRANDING}
+              
+              theme={theme}
+            >
+              {props.children}
+            </NextAppProvider>
+            </React.Suspense>
+          </AppRouterCacheProvider>
+        
+      </body>
+    </html>
+  );
+}
